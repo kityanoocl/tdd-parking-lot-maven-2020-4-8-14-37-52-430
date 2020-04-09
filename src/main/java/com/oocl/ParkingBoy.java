c7d40f4fd85e76989d22f9f106f1b97a46655c2b
Kit Yan <kit.yan@oocl.com>
2020-04-09 17:40:39 +0800

    [Kit] refactor code

--- src/main/java/com/oocl/ParkingBoy.java
+++ src/main/java/com/oocl/ParkingBoy.java
@@ -3,20 +3,29 @@ package com.oocl;
 public class ParkingBoy {
     public final static String UNRECOGNIZED_PARKING_TICKET = "Unrecognized parking ticket.";
     public final static String PLEASE_PROVIDE_YOUR_PARKING_TICKET = "Please provide your parking ticket.";
-    public final static String NOT_ENOUGH_POSITION= "Not enough position.";
+    public final static String NOT_ENOUGH_POSITION = "Not enough position.";
     private ParkingLot parkingLot;
     private String previousErrorMsg;
+
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
 
-    public ParkingTicket park(Car car) {
+    public boolean canParkCar(Car car) {
         boolean isCarNull = car == null;
+        boolean canParkCar = true;
         if (isCarNull || this.parkingLot.isCarParked(car)) {
-            return null;
+            canParkCar = false;
         }
         if (this.parkingLot.isFull()) {
             previousErrorMsg = NOT_ENOUGH_POSITION;
+            canParkCar = false;
+        }
+        return canParkCar;
+    }
+
+    public ParkingTicket park(Car car) {
+        if (!canParkCar(car)) {
             return null;
         }
         ParkingTicket parkingTicket = new ParkingTicket();
@@ -24,14 +33,21 @@ public class ParkingBoy {
         return parkingTicket;
     }
 
-    public Car fetch(ParkingTicket parkingTicket) {
+    public boolean canRetrieveCar(ParkingTicket parkingTicket) {
+        boolean isAbleToRetrieveCar = true;
         if (parkingTicket == null) {
             previousErrorMsg = PLEASE_PROVIDE_YOUR_PARKING_TICKET;
-            return null;
+            isAbleToRetrieveCar = false;
+        } else if (!this.parkingLot.isTicketExist(parkingTicket)) {
+            previousErrorMsg = UNRECOGNIZED_PARKING_TICKET;
+            isAbleToRetrieveCar = false;
         }
 
-        if (!this.parkingLot.isTicketExist(parkingTicket)) {
-            previousErrorMsg = UNRECOGNIZED_PARKING_TICKET;
+        return isAbleToRetrieveCar;
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        if (!canRetrieveCar(parkingTicket)) {
             return null;
         }
         return this.parkingLot.fetch(parkingTicket);
